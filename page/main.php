e7d6d2140cdecad27ac94ae6b9207aa74fc51565
Aaron <aaron.berriche@hotmail.fr>
2021-03-18 15:04:12 +0100

    refonte du site

---
+++ page/main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Online RP</title>
+        <link rel="stylesheet" type="text/css" href="../assets/main.css">
+        <link rel="icon" type="image/jpg" href="../assets/image/logo.jpg" />
+    </head>
+    <body>
+        <header>
+            <div id="up_panel">
+                <h1> test </h1>
+            </div>
+        </header>
+
+        </div>
+            <footer>
+
+            </footer>
+    </body>
+</html>
