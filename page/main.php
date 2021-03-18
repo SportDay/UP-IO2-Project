332c719c8355e49948a646b1c57e29123a790b4e
Aaron <aaron.berriche@hotmail.fr>
2021-03-18 15:33:25 +0100

    php dynamique

--- page/main.php
+++ page/main.php
@@ -5,8 +5,22 @@
         <title>Online RP</title>
         <link rel="stylesheet" type="text/css" href="../assets/main.css">
         <link rel="icon" type="image/jpg" href="../assets/image/logo.jpg" />
+        <style> 
+            body
+            {
+                <?php
+                    // injecter du css dans une page html est de manière general une mauvaise pratique
+                    // cependant dans le cadre de ce projet le css des pages n'est pas statique
+                    // les elements dynamiques du php sont donc fait en php 
+                    
+                    //echo "background-color:black";
+                ?>
+            }
+        </style>
     </head>
     <body>
+        <php
+
         <header>
             <div id="up_panel">
                 <h1> test </h1>
