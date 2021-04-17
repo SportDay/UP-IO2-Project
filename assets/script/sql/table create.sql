ba7ede6b9ab84669307696b24301a81157c9e2ea
SportDay <49455066+SportDay@users.noreply.github.com>
2021-04-17 20:05:05 +0200

    update

--- assets/script/sql/table create.sql
+++ assets/script/sql/table create.sql
@@ -6,8 +6,6 @@ drop table if exists users;
  
 CREATE TABLE `users` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
-`firstname` varchar(64) DEFAULT NULL,
-`lastname` varchar(64) DEFAULT NULL,
 `username` varchar(64) DEFAULT NULL,
 `password` varchar(64) DEFAULT NULL,
 `publicname` varchar(64) DEFAULT NULL,
@@ -17,8 +15,6 @@ CREATE TABLE `users` (
 `enablepublic` varchar(64) DEFAULT NULL,
 `banned` varchar(64) DEFAULT FALSE,
 `bannedto` varchar(64) DEFAULT NULL,
-`follows` varchar(64) DEFAULT NULL,
-`secretwords` varchar(64) DEFAULT NULL,
 `admin` varchar(64) DEFAULT FALSE,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
@@ -28,15 +24,13 @@ drop table if exists posts;
  
 CREATE TABLE `posts` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
-`author` varchar(64) DEFAULT NULL,
-`username` varchar(64) DEFAULT NULL,
 `userid` varchar(64) DEFAULT NULL,
 `reported` varchar(64) DEFAULT FALSE,
 `reportnum` varchar(64) DEFAULT 0,
-`reporteddate` varchar(64) DEFAULT NULL,
+`lastreport` varchar(64) DEFAULT NULL,
 `creationdate` varchar(64) DEFAULT NULL,
-`title` varchar(64) DEFAULT NULL,
 `content` varchar(64) DEFAULT NULL,
 `likenum` varchar(64) DEFAULT 0,
+`reponseId` varchar(64) DEFAULT -1,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
