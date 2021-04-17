e84b54ca4d27c20e104cf9f97ec862a00b2949a4
Ophiase <aaron.berriche@hotmail.fr>
2021-04-17 08:56:05 +0200

    Page Accueil

--- root_public/page/public/home.php
+++ root_public/page/public/home.php
@@ -14,12 +14,19 @@
 ?>
 <!-- ------------------------------------------ -->
 
-  <?php
-
-      for ($i = 0; $i < 50; $i++)
-        write("test : " . $i);
-
-  ?>
+  <h1>Harry Potter | Role Play</h1>
+  <?php separator(); ?>
+  <pre>
+      Bienvenue sur la communauté n°1 en france de jeux de role autour de l'univers d'Harry Potter.
+      Enfilez votre cap et votre chapeau, attrapez une baguette. Une grande aventure en temps réel vous attends.
+  </pre>
+  <?php separator(); ?>
+  <h2>Fonctionnement du site.</h2>
+  <pre>
+    Chaque compte possède une partie ...
+  </pre>
+  
+  
 
 <!-- ------------------------------------------ -->
 <?php require($global_params["root"] . "assets/script/php/footer.php"); ?>
