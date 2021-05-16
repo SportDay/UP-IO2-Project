8f41f66ae068736518ca87357052d85e858735ee
Ophiase <aaron.berriche@hotmail.fr>
2021-05-17 00:21:01 +0200

    clean up test_debug

--- root_public/page/test_debug/test.php
+++ root_public/page/test_debug/test.php
@@ -5,36 +5,9 @@
         "root_public" => "../../",
     ];
 
-    require("../../../" . "assets/script/php/" . "functions.php");
-    require($global_params["root"] . "assets/script/php/constants.php");
+    require_once($global_params["root"] . "assets/script/php/"."functions.php");
+    require_once($global_params["root"] . "assets/script/php/"."constants.php");
 
-    /*
-    $rows = [
-        "id"=>29113231422224322,
-        "creation_date"=>2223122315332,
-        "username"=>"Mazrazzaatin"
-    ];
+    write("Test");
 
-    separator();
-    write(hashPassword("s1922aat_ca_va?", $rows));
-
-    separator();
-
-    $str = bin2hex(random_bytes(20));
-    write($str);
-    */
-
-    //$connexion = mysqli_connect("127.0.0.1", "root", "", "reseau");
-
-    //$query = "INSERT INTO `users` (`username`,    `password`) VALUES " .
-    //  "(\"" . $connexion->real_escape_string("toast") . "\", \"none\");";
-
-    //print_r($connexion->query($query));
-
-    //write($h);
-
-    //gettype(hashPassword("test", ["id"=>"3", "creation_date"=>"19291", "username"=>"jean"]));
-
-    for ($i = 0; $i < 10; $i++)
-        write(json_encode(generateRandomPublicData()));
 ?>
